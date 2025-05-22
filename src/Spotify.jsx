15ca4def265174f40e702d9bf54cf2584d83587c
lijinxuan <jinxuan.jn.li@gmail.com>
2025-05-22 17:25:19 +0800

    add test code

--- src/Spotify.jsx
+++ src/Spotify.jsx
@@ -1,33 +1,59 @@
 import React from "react";
 
-function Spotify() {
+const getSpotifyToken = async () => {
     const spotifyBaseUrl = "https://accounts.spotify.com";
+    const spotifyEndpoint = "/api/token";
+    const contentType = "application/x-www-form-urlencoded";
+    const clientId = "your client id here";
+    const clientSecret = "your client secret here";
 
-    const getToken = async () => {
-        const spotifyEndpoint = "/api/token";
-        const contentType = "application/x-www-form-urlencoded";
-        const clientId = "your client id here";
-        const clientSecret = "your client secret here";
+    try {
 
-        try {
+        const response = await fetch(spotifyBaseUrl + spotifyEndpoint, {
+            method: "POST",
+            headers: {
+                "Content-Type": contentType,
+            },
+            body: `grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}`
+        })
+        if (response.ok) {
+            const jsonObj = await response.json();
+            // alert(jsonObj["access_token"]);
 
-            const response = await fetch(spotifyBaseUrl + spotifyEndpoint, {
-                method: "POST",
-                headers: {
-                    "Content-Type": contentType,
-                },
-                body: `grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}`
-            })
-            if(response.ok){
-                const jsonObj = await response.json();
-                alert(jsonObj["access_token"]);
-            }
-        } catch (error) {
-            console.log(error);
+            return jsonObj["access_toekn"];
         }
+    } catch (error) {
+        console.log(error);
     }
-    return <button onClick={getToken}>Get Token</button>;
 
 }
 
-export default Spotify;
+const getSpotifyTracks = async (token, searchTerms) => {
+
+    const endpoint = 'https://api.spotify.com/v1/search';
+    const params = new URLSearchParams({
+        q: searchTerms,
+        type: "track",
+        market: "ES",
+        limit: 5,
+        offset: 0
+    })
+
+    const queryUrl = `${endpoint}?${params.toString()}`;
+
+    const response = await fetch(queryUrl, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`
+        }
+    })
+
+
+
+
+
+
+}
+
+export { getSpotifyToken };
---
+++ test/testSpotify.test.js
@@ -0,0 +1,13 @@
+import { describe, it } from "vitest";
+import { getSpotifyTrack } from "./src/Spotify"
+
+describe("getSpotifyTrack", () => {
+
+   it('no token', () => {
+
+      const expect = 
+
+
+   });
+
+})
